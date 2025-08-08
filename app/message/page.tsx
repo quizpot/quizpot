cfc6299eab3bf3c75f6561400b8037c4dbece19b
kragleh <kragleh@gmail.com>
2025-08-08 17:28:15 +0200

    Fix Message Use Effect

--- app/message/page.tsx
+++ app/message/page.tsx
@@ -1,16 +1,28 @@
 "use client"
 import Button from '@/components/ui/Button'
 import { redirect } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const MessagePage = () => {
-  const messageTitle = sessionStorage.getItem('messageTitle')
-  const messageDescription = sessionStorage.getItem('messageDescription')
+  const [messageTitle, setMessageTitle] = useState<string | null>(null)
+  const [messageDescription, setMessageDescription] = useState<string | null>(null)
+
+  useEffect(() => {
+    const mt = sessionStorage.getItem('messageTitle')
+    const md = sessionStorage.getItem('messageDescription')
+
+    if (!mt || !md) {
+      sessionStorage.removeItem('messageTitle')
+      sessionStorage.removeItem('messageDescription')
+      redirect('/')
+    }
+
+    setMessageTitle(mt)
+    setMessageDescription(md)
+  }, [])
 
   if (!messageTitle || !messageDescription) {
-    sessionStorage.removeItem('messageTitle')
-    sessionStorage.removeItem('messageDescription')
-    redirect('/')
+    return null
   }
 
   return (

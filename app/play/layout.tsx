55f0514fd752761f74d828233cc97449970ff486
kragleh <kragleh@gmail.com>
2025-08-09 13:55:08 +0200

    Suspense Gibberish

--- app/play/layout.tsx
+++ app/play/layout.tsx
@@ -1,14 +1,16 @@
 import ConnectionStatus from '@/components/play/ConnectionStatus'
 import { LobbyStateProvider } from '@/components/providers/LobbyStateProvider'
 import { WebSocketProvider } from '@/components/ws/WebSocket'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const PlayLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <WebSocketProvider>
       <LobbyStateProvider>
         <ConnectionStatus />
-        { children }
+        <Suspense>
+          { children }
+        </Suspense>
       </LobbyStateProvider>
     </WebSocketProvider>
   )

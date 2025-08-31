dd5de5cb20c9cb9973a9a0c8cfcfcaa8dcca544b
kragleh <kragleh@gmail.com>
2025-08-31 14:00:11 +0200

    Suspense Back

--- app/play/layout.tsx
+++ app/play/layout.tsx
@@ -1,14 +1,16 @@
 import ConnectionStatus from '@/components/ui/ConnectionStatus'
 import { PlayerLobbyStateProvider } from '@/components/providers/PlayerLobbyStateProvider'
 import { WebSocketProvider } from '@/components/providers/WebSocketProvider'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const PlayerLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <WebSocketProvider>
       <PlayerLobbyStateProvider>
         <ConnectionStatus />
-        { children }
+        <Suspense>
+          { children }
+        </Suspense>
       </PlayerLobbyStateProvider>
     </WebSocketProvider>
   )

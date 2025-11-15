279eccf6805ef7c1ef2015904981dfb2324be209
kragleh <kragleh@gmail.com>
2025-11-15 15:57:18 +0100

    Header Enhancement

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -1,37 +1,86 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Notification from './Notification'
 import FancyButton from '../ui/fancy-button'
 import Link from 'next/link'
 import { ThemeSwitch } from '../ui/theme-switch'
+import { createPortal } from 'react-dom'
+import { Menu } from 'lucide-react'
+import { useMessages } from 'next-intl'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const menu: { label: string, href: string }[] = []
+  const messages = useMessages()
+  const keys = Object.keys(messages.Header)
+
+  for (const key of keys) {
+    menu.push({
+      label: messages.Header[key].label,
+      href: messages.Header[key].href
+    })
+  }
+
   return (
-    <header className='fixed top-0 left-0 w-full z-10 bg-white/60 dark:bg-black/60 backdrop-blur-sm'>
-      <Notification>
-        This is an early version of Quizpot under active development. <b>Expect frequent changes.</b> Download quizzes as a file for long term storage.
-      </Notification>
-      <div className='container w-full mx-auto p-4 flex justify-between select-none'>
-        <FancyButton asChild>
-          <Link href={'/'} className='text-2xl font-semibold'>
-            Quizpot
-          </Link>
-        </FancyButton>
-        <div className='flex gap-8 items-center justify-center text-xl'>
-          <FancyButton size='sm' asChild>
-            <Link href={'/stats'} className='text-lg'>
-              Stats
+    <>
+      <header className='fixed top-0 left-0 w-full z-10 bg-white/60 dark:bg-black/60 backdrop-blur-sm'>
+        <Notification>
+          This is an early version of Quizpot under active development. <b>Expect frequent changes.</b> Download quizzes as a file for long term storage.
+        </Notification>
+        <div className='container w-full mx-auto p-4 flex justify-between select-none'>
+          <FancyButton asChild>
+            <Link href={'/'} className='text-2xl font-semibold'>
+              Quizpot
             </Link>
           </FancyButton>
-          <FancyButton size='sm' asChild>
-            <Link href={'https://github.com/quizpot'} className='text-lg'>
-              GitHub
-            </Link>
-          </FancyButton>
-          <ThemeSwitch />
+          <div className='hidden md:flex gap-8 items-center justify-center text-lg'>
+            {
+              menu.map(({ href, label }, index) => (
+                <FancyButton key={ index } size='sm' asChild>
+                  <Link href={ href }>
+                    { label }
+                  </Link>
+                </FancyButton>
+              ))
+            }
+            <ThemeSwitch />
+          </div>
+          <div className='flex md:hidden items-center justify-between'>
+            <FancyButton className='block' onClick={ () => setIsOpen(true) }>
+              <Menu size={ 20 } />
+            </FancyButton>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      { isOpen && <MobileMenu menu={ menu } setIsOpen={ setIsOpen } /> }
+    </>
   )
+}
+
+const MobileMenu = ({ menu, setIsOpen }: { menu: { label: string, href: string }[], setIsOpen: React.Dispatch<React.SetStateAction<boolean>> }) => {
+  return createPortal(
+    <section 
+      className='
+        z-50 fixed left-0 top-0 h-screen w-full backdrop-blur-sm bg-white/80 dark:bg-black/80
+        flex md:hidden flex-col items-center justify-center gap-4 p-4
+      ' 
+      onClick={ () => setIsOpen(false) }
+    >
+      {
+        menu.map(({ href, label }, index) => (
+          <FancyButton key={ index } className='w-full text-center text-2xl' asChild>
+            <Link href={ href }>
+              { label }
+            </Link>
+          </FancyButton>
+        ))
+      }
+      <div className='flex items-center justify-center mt-8'>
+        <ThemeSwitch />
+      </div>
+    </section>
+  , document.body)
 }
 
 export default Header

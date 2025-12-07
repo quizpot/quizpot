cbf8843a43dad611c37c2d4295a524aa1efe38c5
kragleh <kragleh@gmail.com>
2025-12-07 21:17:30 +0100

    Play Page Header

--- components/player/pages/SetCodePage.tsx
+++ components/player/pages/SetCodePage.tsx
@@ -4,6 +4,7 @@ import NumberInput from '../../ui/number-input'
 import { useTranslations } from 'next-intl'
 import FancyButton from '@/components/ui/fancy-button'
 import Link from 'next/link'
+import Header from '@/components/nav/Header'
 
 const SetCodePage = () => {
   const t = useTranslations('PlayPage')
@@ -11,24 +12,27 @@ const SetCodePage = () => {
   const [code, setCode] = React.useState<number>(0)
 
   return (
-    <section className='flex flex-col gap-4 items-center justify-center h-screen w-full p-4'>
-      <h1 className='text-2xl font-semibold'>{ t('title') }</h1>
-      <div className='max-w-md'>
-        <NumberInput onChange={(e) => {
-          const value = e.target.value
-          if (value === '') {
-            setCode(0)
-          } else {
-            setCode(parseInt(e.target.value))
-          }
-        }} value={ code } />
-      </div>
-      <FancyButton color='green' asChild>
-        <Link href={'./play?code=' + code}>
-          { t('continue') }
-        </Link>
-      </FancyButton>
-    </section>
+    <>
+      <Header />
+      <section className='flex flex-col gap-4 items-center justify-center h-screen w-full p-4'>
+        <h1 className='text-2xl font-semibold'>{ t('title') }</h1>
+        <div className='max-w-md'>
+          <NumberInput onChange={(e) => {
+            const value = e.target.value
+            if (value === '') {
+              setCode(0)
+            } else {
+              setCode(parseInt(e.target.value))
+            }
+          }} value={ code } />
+        </div>
+        <FancyButton color='green' asChild>
+          <Link href={'./play?code=' + code}>
+            { t('continue') }
+          </Link>
+        </FancyButton>
+      </section>
+    </>
   )
 }
 

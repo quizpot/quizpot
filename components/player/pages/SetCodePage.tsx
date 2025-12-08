57ca6ee10aef8b947219ec77a8dba14a08f82b12
kragleh <kragleh@gmail.com>
2025-12-08 08:46:00 +0100

    Enhance Code Input

--- components/player/pages/SetCodePage.tsx
+++ components/player/pages/SetCodePage.tsx
@@ -1,15 +1,35 @@
 "use client"
-import React from 'react'
-import NumberInput from '../../ui/number-input'
+import React, { useEffect } from 'react'
 import { useTranslations } from 'next-intl'
 import FancyButton from '@/components/ui/fancy-button'
 import Link from 'next/link'
 import Header from '@/components/nav/Header'
+import TextInput from '@/components/ui/TextInput'
 
 const SetCodePage = () => {
   const t = useTranslations('PlayPage')
 
-  const [code, setCode] = React.useState<number>(0)
+  const [code, setCode] = React.useState<string>('')
+
+  useEffect(() => {
+    const submit = () => {
+      window.location.href = '/play?code=' + code
+    }
+
+    window.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        submit()
+      }
+    })
+
+    return () => {
+      window.removeEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+          submit()
+        }
+      })
+    }
+  }, [code])
 
   return (
     <>
@@ -17,14 +37,16 @@ const SetCodePage = () => {
       <section className='flex flex-col gap-4 items-center justify-center h-screen w-full p-4'>
         <h1 className='text-2xl font-semibold'>{ t('title') }</h1>
         <div className='max-w-md'>
-          <NumberInput onChange={(e) => {
-            const value = e.target.value
-            if (value === '') {
-              setCode(0)
-            } else {
-              setCode(parseInt(e.target.value))
-            }
-          }} value={ code } />
+          <TextInput 
+            onChange={(e) => {
+              const value = e.target.value
+              const isOnlyDigits = /^\d{0,6}$/.test(value)
+
+              if (isOnlyDigits) {
+                setCode(value)
+              }
+            }}
+          value={ code } />
         </div>
         <FancyButton color='green' asChild>
           <Link href={'./play?code=' + code}>

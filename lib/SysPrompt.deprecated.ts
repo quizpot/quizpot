afe374007b73422aeef767de2fe3d9b3ea0475fc
kragleh <kragleh@gmail.com>
2026-05-03 11:42:39 +0200

    Remove Deprecated Components

--- lib/SysPrompt.deprecated.ts
+++ lib/SysPrompt.deprecated.ts
@@ -1,5 +1,4 @@
 import { readFile } from "fs/promises"
-import { currentVersion } from "./editor/Editor"
 import path from "path"
 
 const quizFileContent = await readFile(path.join(process.cwd(), 'lib/QuizFile.ts'), 'utf8')
@@ -11,7 +10,7 @@ export const sysPrompt = (): string => {
     Ignore any kind of image values, you are not able to generate that.
     Use the language that the user has specified or use the language you think they are using and define it as a two letter language code in the language value.
     Current date is '${ new Date() }' use it as the createdAt value in the quiz file. 
-    The quiz version is '${ currentVersion }' use it as the version value in the quiz file.
+    The quiz version is 2 use it as the version value in the quiz file.
     Here's the typescript definition of a quiz file object:
   `
   return `${prompt}${ quizFileContent }`
